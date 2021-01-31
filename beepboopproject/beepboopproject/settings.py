d500af4f90d1142fe8e1279274027f7f76cecb05
scheung <scheung97@DESKTOP-MLG4DJ9.localdomain>
2021-01-30 20:21:42 -0500

    fixed bug with secret key not being imported

--- beepboopproject/beepboopproject/settings.py
+++ beepboopproject/beepboopproject/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import config as cfig
+import os 
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = cfig.django_secretkey
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
